475ae52c7f89b44111df6275e576360ca35e53df
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-23 12:28:42 +0500

    restore the home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import { FeatureTitle, Container } from '@/components'
-import { featureTitles } from '@/data/featureTitleData'
-
 export default function Home() {
-  return (
-    <main>
-      <Container>
-        <div>
-          {featureTitles.map((item, index) => (
-            <FeatureTitle
-              key={index}
-              icon={item.icon}
-              title={item.title}
-              description={item.description}
-            />
-          ))}
-        </div>
-      </Container>
-    </main>
-  )
+  return <main></main>
 }
