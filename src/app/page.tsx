81b8a5fec32f3c16b403f799482ca54eb7517fe4
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-23 18:24:50 +0500

    redo the changes in  home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import { Button } from '@/components'
-import { ArrowUpRight } from '@/icons'
 
 export default function Home() {
-  return (
-    <main>
-      
-    </main>
-  )
+  return <main></main>
 }
