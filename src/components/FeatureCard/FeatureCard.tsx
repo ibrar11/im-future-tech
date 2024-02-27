6779edb2853fbc65debe0478d31a10d0ea2a667c
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-27 19:08:38 +0500

    resolve comment.

--- src/components/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -10,7 +10,7 @@ const FeatureCard = (props: FeatureCardProps) => {
   const { title = '', description = '', styles = '' } = props
   return (
     <div
-      className={`flex flex-col gap-y-1 rounded-xl bg-dark-300 p-6 md:gap-y-4 md:p-8 2xl:gap-y-5 2xl:p-10 ${styles}`}
+      className={`flex flex-col gap-y-1 rounded-xl border-2 border-solid border-dark-400 bg-dark-300 p-6 md:gap-y-4 md:p-8 2xl:gap-y-5 2xl:p-10 ${styles}`}
     >
       <p className="text-lg font-medium text-white md:text-xl 2xl:text-2xl">
         {title}
