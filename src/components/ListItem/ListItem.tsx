c2299e2bb3cad153f517b6b92cc881f3e6982b6c
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-27 12:22:56 +0500

    resolve comments about path and alt props.

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -4,19 +4,23 @@ type ListItemProps = {
   styles: string
   title: string
   description: string
+  path: string
+  alt: string
 }
 
 const ListItem = (props: ListItemProps) => {
-  const { styles = '', title = '', description = '' } = props
+  const {
+    styles = '',
+    title = '',
+    description = '',
+    path = '',
+    alt = '',
+  } = props
   return (
     <div
       className={`flex items-center gap-x-2 md:gap-x-2.5 2xl:gap-x-4 ${styles}`}
     >
-      <img
-        src="/images/jhonTechson.png"
-        alt="personImage"
-        className="size-15 rounded-full 2xl:size-20"
-      />
+      <img src={path} alt={alt} className="size-15 rounded-full 2xl:size-20" />
       <div className="flex flex-col 2xl:gap-y-0.5">
         <p className="text-lg font-semibold text-white md:text-xl 2xl:text-2xl">
           {title}
