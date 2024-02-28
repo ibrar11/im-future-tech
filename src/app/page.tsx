621a95e18e114eb9d8438cd90c172d84ce681bc4
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-28 20:00:08 +0500

    recreate the button component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
 import React from 'react'
-import { Container, Button } from '@/components'
-import { Eye } from '@/icons'
 
 export default function Home() {
-  return (
-    <main>
-      <Container>
-        <div className="flex gap-x-5">
-          <Button icon={<Eye />}>Preview</Button>
-          <Button variant="Secondary">Contact Us</Button>
-          <Button icon={<Eye color="black" />} variant="Secondary">
-            Preview
-          </Button>
-        </div>
-      </Container>
-    </main>
-  )
+  return <main></main>
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,68 +1,64 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
+import { tw } from '@/utils/Tw'
 
 type ButtonProps = {
   icon?: React.ReactNode
-  styles?: string
   variant?: string
   href?: string
+  fullWidth?: boolean
+  disabled?: boolean
+  onClick?: (...args: any[]) => unknown
   children?: string | React.ReactNode
 }
 
 const Button = (props: ButtonProps) => {
-  const { icon, styles, variant = 'Primary', children, href } = props
+  const {
+    icon,
+    variant = 'Primary',
+    children,
+    href,
+    fullWidth,
+    disabled,
+    onClick,
+  } = props
 
-  const primaryButton = href ? (
-    <Link
-      href={href}
-      className={`flex w-max cursor-pointer items-center justify-center rounded-xl border-2 border-solid border-dark-400 px-5 py-3.5 text-sm font-normal text-grey-300 hover:bg-dark-400 hover:text-grey-500 focus:bg-dark-500 focus:text-grey-600 active:bg-dark-300 active:text-grey-400 2xl:text-lg ${styles}`}
-    >
-      {children && children}
+  const btnDisabled = href?.length && disabled ? false : disabled ? true : false
+
+  const primaryButton = btnDisabled
+    ? tw`cursor-not-allowed bg-dark-500 text-grey-500 focus:ring-0`
+    : tw`border-2 border-solid border-dark-400  text-grey-300 transition-all hover:bg-dark-400 hover:text-grey-500 focus:border-dark-700 focus:text-grey-600 focus:ring focus:ring-dark-700 active:bg-dark-300 active:text-grey-400`
+
+  const secondayButton = btnDisabled
+    ? tw`cursor-not-allowed bg-yellow-500 text-dark-500 focus:ring-0`
+    : tw`border-2 border-solid border-dark-200 bg-yellow-200 text-dark-200 hover:bg-yellow-400 hover:text-dark-400 focus:border-yellow-500 focus:text-dark-500 focus:ring focus:ring-yellow-500 active:bg-yellow-300 active:text-dark-300`
+
+  const btnVariant = variant === 'Secondary' ? secondayButton : primaryButton
+
+  const buttonProps = {
+    className: tw`flex cursor-pointer items-center justify-center rounded-md px-5 py-3.5 text-sm font-normal outline-none transition-all 2xl:text-lg ${btnVariant} ${fullWidth ? 'w-full' : ''}`,
+    'aria-disabled': btnDisabled ? true : undefined,
+    'aria-label': children,
+    onClick: onClick,
+  }
+
+  const linkProps = { ...buttonProps, href }
+
+  const buttonElement = href?.length ? Link : 'button'
+
+  return React.createElement(
+    buttonElement,
+    href ? linkProps : (buttonProps as any),
+    <>
+      {children}
       {icon && (
         <span className="ml-1 flex size-5 items-center justify-center 2xl:ml-2.5 2xl:size-6">
           {icon}
         </span>
       )}
-    </Link>
-  ) : (
-    <button
-      className={`flex cursor-pointer items-center justify-center rounded-xl border-2 border-solid border-dark-400 px-5 py-3.5 text-sm font-normal text-grey-300 hover:bg-dark-400 hover:text-grey-500 focus:bg-dark-500 focus:text-grey-600 active:bg-dark-300 active:text-grey-400 2xl:text-lg ${styles}`}
-    >
-      {children && children}
-      {icon && (
-        <span className="ml-1 flex size-5 items-center justify-center 2xl:ml-2.5 2xl:size-6">
-          {icon}
-        </span>
-      )}
-    </button>
+    </>,
   )
-
-  const secondayButton = href ? (
-    <Link
-      href={href}
-      className={`flex w-max cursor-pointer items-center justify-center rounded-xl bg-yellow-200 px-5 py-3.5 text-sm font-normal text-dark-200 hover:bg-yellow-400 hover:text-dark-400 focus:bg-yellow-500 focus:text-dark-500 focus:ring-2 focus:ring-grey-300 active:bg-yellow-300 active:text-dark-300 2xl:text-lg ${styles}`}
-    >
-      {children && children}
-      {icon && (
-        <span className="ml-1 flex size-5 items-center justify-center 2xl:ml-2.5 2xl:size-6">
-          {icon}
-        </span>
-      )}
-    </Link>
-  ) : (
-    <button
-      className={`flex cursor-pointer items-center justify-center rounded-xl bg-yellow-200 px-5 py-3.5 text-sm font-normal text-dark-200 hover:bg-yellow-400 hover:text-dark-400 focus:bg-yellow-500 focus:text-dark-500 focus:ring-2 focus:ring-grey-300 active:bg-yellow-300 active:text-dark-300 2xl:text-lg ${styles}`}
-    >
-      {children && children}
-      {icon && (
-        <span className="ml-1 flex size-5 items-center justify-center 2xl:ml-2.5 2xl:size-6">
-          {icon}
-        </span>
-      )}
-    </button>
-  )
-
-  return variant === 'Secondary' ? secondayButton : primaryButton
 }
 
 export default Button
