6ce0ffea6bc7053601e1cda0c21f3c79f34e1724
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-28 15:05:18 +0500

    fix the reusability of button component.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,33 +1,68 @@
+import Link from 'next/link'
 import React from 'react'
 
 type ButtonProps = {
   icon?: React.ReactNode
-  text: string
-  iconStyles?: string
-  textStyles: string
-  styles: string
+  styles?: string
+  variant?: string
+  href?: string
+  children?: string | React.ReactNode
 }
 
 const Button = (props: ButtonProps) => {
-  const {
-    text = '',
-    icon = '',
-    iconStyles = '',
-    textStyles = '',
-    styles = '',
-  } = props
-  return (
-    <button
-      className={`flex items-center justify-center rounded-xl px-5 py-3.5 ${styles}`}
+  const { icon, styles, variant = 'Primary', children, href } = props
+
+  const primaryButton = href ? (
+    <Link
+      href={href}
+      className={`flex cursor-pointer items-center justify-center rounded-xl border-2 border-solid border-dark-400 px-5 py-3.5 text-sm font-normal text-grey-300 hover:bg-dark-400 hover:text-grey-500 focus:bg-dark-500 focus:text-grey-600 active:bg-dark-300 active:text-grey-400 2xl:text-lg ${styles}`}
     >
-      <p className={`text-sm font-normal 2xl:text-lg ${textStyles}`}>{text}</p>
-      <span
-        className={`flex w-5 items-center justify-center p-[2px] 2xl:w-6 ${iconStyles}`}
-      >
-        {icon}
-      </span>
+      {children && children}
+      {icon && (
+        <span className="ml-1 flex size-5 items-center justify-center 2xl:ml-2.5 2xl:size-6">
+          {icon}
+        </span>
+      )}
+    </Link>
+  ) : (
+    <button
+      className={`flex cursor-pointer items-center justify-center rounded-xl border-2 border-solid border-dark-400 px-5 py-3.5 text-sm font-normal text-grey-300 hover:bg-dark-400 hover:text-grey-500 focus:bg-dark-500 focus:text-grey-600 active:bg-dark-300 active:text-grey-400 2xl:text-lg ${styles}`}
+    >
+      {children && children}
+      {icon && (
+        <span className="ml-1 flex size-5 items-center justify-center 2xl:ml-2.5 2xl:size-6">
+          {icon}
+        </span>
+      )}
     </button>
   )
+
+  const secondayButton = href ? (
+    <Link
+      href={href}
+      className={`flex w-max cursor-pointer items-center justify-center rounded-xl bg-yellow-200 px-5 py-3.5 text-sm font-normal text-dark-200 hover:bg-yellow-400 hover:text-dark-400 focus:bg-yellow-500 focus:text-dark-500 focus:ring-2 focus:ring-grey-300 active:bg-yellow-300 active:text-dark-300 2xl:text-lg ${styles}`}
+    >
+      {children && children}
+      {icon && (
+        <span className="ml-1 flex size-5 items-center justify-center 2xl:ml-2.5 2xl:size-6">
+          {icon}
+        </span>
+      )}
+    </Link>
+  ) : (
+    <button
+      className={`flex cursor-pointer items-center justify-center rounded-xl bg-yellow-200 px-5 py-3.5 text-sm font-normal text-dark-200 hover:bg-yellow-400 hover:text-dark-400 focus:bg-yellow-500 focus:text-dark-500 focus:ring-2 focus:ring-grey-300 active:bg-yellow-300 active:text-dark-300 2xl:text-lg ${styles}`}
+    >
+      {children && children}
+      {icon && (
+        <span className="ml-1 flex size-5 items-center justify-center 2xl:ml-2.5 2xl:size-6">
+          {icon}
+        </span>
+      )}
+    </button>
+  )
+
+  return variant === 'Secondary' ? secondayButton : primaryButton
 }
 
 export default Button
