07eb82fa00ea82897f3eb92bce62c5930b94e216
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-29 13:02:26 +0500

    resolve the border issue on active class.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import { Container, Button } from '@/components'
+import { Eye } from '@/icons'
 import React from 'react'
 
 export default function Home() {
-  return <main></main>
+  return (
+    <main>
+      <Container>
+        <div className="flex gap-x-5">
+          <Button icon={<Eye />} disabled>
+            Preview
+          </Button>
+          <Button icon={<Eye />} variant="Secondary" disabled>
+            Preview
+          </Button>
+          <Button variant="Secondary">Preview</Button>
+        </div>
+      </Container>
+    </main>
+  )
 }
