d1a07c5fe6bc1bd8acbd87f2e3202a3f4525c645
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-29 18:06:06 +0500

    remove the components from home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import { Container, FooterListItem } from '@/components'
-import { footerListData } from '@/components/FooterListItem/footerList'
 import React from 'react'
 
 export default function Home() {
-  return (
-    <main>
-      <Container>
-        <div>
-          {footerListData.map((item) => (
-            <FooterListItem
-              key={item.id}
-              title={item.title}
-              subTitles={item.subTitles}
-            />
-          ))}
-        </div>
-      </Container>
-    </main>
-  )
+  return <main></main>
 }
