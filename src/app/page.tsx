afb8239d166b785c3b8a1f9f26b37f455ef85c62
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-29 13:25:26 +0500

    remove the components from the home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Container, StarRatingItem } from '@/components'
 
 export default function Home() {
   return (
     <main>
-      <Container>
-        <StarRatingItem />
-      </Container>
     </main>
   )
 }
