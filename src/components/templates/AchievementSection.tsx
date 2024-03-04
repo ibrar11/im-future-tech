791294343257e03dc348a6c31b5f2c1d658bdf86
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-03-04 19:45:41 +0500

    create the features header section.

--- src/components/templates/AchievementSection.tsx
+++ src/components/templates/AchievementSection.tsx
@@ -15,7 +15,7 @@ const AchievementSection = () => {
               title={item.title}
               description={item.description}
               subHeading={item.subHeading}
-              styles={tw`border-t-2 border-dark-400 ${index === 0 ? 'md:pr-2 lg:pr-10 xl:pr-20 2xl:pr-28' : index === achievements.length - 1 ? 'md:grow md:pl-2 border-l-2 border-dark-400 lg:pl-10 xl:pl-12.5 2xl:pl-28' : '2xl:px-28 xl:px-20 lg:px-10 md:px-2 border-l-2 border-dark-400'}`}
+              styles={tw`border-t-2 border-dark-400 ${index === 0 ? 'md:pr-2 lg:pr-10 xl:pr-20 2xl:pr-28' : index === achievements.length - 1 ? 'md:grow md:pl-2 md:border-l-2 border-dark-400 lg:pl-10 xl:pl-12.5 2xl:pl-28' : '2xl:px-28 xl:px-20 lg:px-10 md:px-2 md:border-l-2 border-dark-400'}`}
             />
           ))}
         </div>
