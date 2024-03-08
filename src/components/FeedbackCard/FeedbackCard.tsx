1a6dd14ea1446d384a048f2e287c7f15cb4bff0c
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-03-08 12:04:04 +0500

    fix the bottom border of feedback cards.

--- src/components/FeedbackCard/FeedbackCard.tsx
+++ src/components/FeedbackCard/FeedbackCard.tsx
@@ -10,7 +10,7 @@ const FeedbackCard = (props: FeedbackCardProps) => {
   const { feedback } = props
   return (
     <div
-      className={`gap-y-7.5 sm:py-15 flex flex-col items-center justify-start border-dark-400 py-10 ${feedback.id % 2 === 0 ? 'sm:border-l-2 sm:pl-10' : 'sm:pr-10'} 2xl:gap-y-10 ${feedback.id > 3 ? 'hidden sm:flex' : feedback.id === 3 ? 'border-b-0' : 'border-b-2'} ${feedback.id === 1 || feedback.id === 4 ? 'lg:border-x-0 lg:pr-10' : feedback.id === 2 || feedback.id === 5 ? 'lg:border-l-2 lg:px-10' : 'lg:border-l-2 lg:pl-10'}`}
+      className={`gap-y-7.5 sm:py-15 flex flex-col items-center justify-start border-dark-400 py-10 ${feedback.id % 2 === 0 ? 'sm:border-l-2 sm:pl-10' : 'sm:pr-10'} 2xl:gap-y-10 ${feedback.id > 4 ? '' : feedback.id > 3 ? 'hidden sm:flex sm:border-b-2 lg:border-b-0' : feedback.id === 3 ? 'border-b-0 sm:border-b-2' : 'border-b-2'} ${feedback.id === 1 || feedback.id === 4 ? 'lg:border-x-0 lg:pr-10' : feedback.id === 2 || feedback.id === 5 ? 'lg:border-l-2 lg:px-10' : 'lg:border-l-2 lg:pl-10'}`}
     >
       <ListItem
         title={feedback.username}
