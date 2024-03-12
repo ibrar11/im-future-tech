9c02779f4ee82bff4eb18aed75b2ee6bcad72cf8
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-03-12 12:16:41 +0500

    make the header logo link to home page.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation'
 import { DuoCircularBlade } from '@/icons'
 import { Button, Container, HamburgerIcon, Links } from '..'
 import { links } from '../NavLinks/navLinks'
+import Link from 'next/link'
 
 const NavBar = () => {
   const [dropDown, setDropDown] = useState(false)
@@ -37,12 +38,12 @@ const NavBar = () => {
     <nav className="border-y-2 border-dark-400 bg-dark-300">
       <Container>
         <div className="relative flex items-center py-5 md:justify-between md:p-0">
-          <div className="flex items-center gap-x-2">
+          <Link href={'/'} className="flex items-center gap-x-2">
             <DuoCircularBlade color="#FFD11A" />
             <h2 className="text-xl font-semibold text-white 2xl:text-2xl">
               FutureTech
             </h2>
-          </div>
+          </Link>
           <HamburgerIcon
             isNavOpened={dropDown}
             handleDropDown={handleDropDown}
