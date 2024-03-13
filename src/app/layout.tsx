290a7f6f5c56cddc11a2456dac866bc8c5ade685
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-03-13 11:20:29 +0500

    create the news page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Footer, Header } from '@/components'
+import { Header, Footer } from '@/components'
 
 export const metadata: Metadata = {
   title: 'FutureTech',
---
+++ src/app/news/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) => {
+  return <>{children}</>
+}
+
+export default NewsLayout
