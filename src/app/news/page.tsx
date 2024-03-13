f38535853ad999ea4b0dbfdba174a84c661caae3
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-03-13 11:56:13 +0500

    create the news headline section.

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { HeadlineSection } from './newsComponents'
 
 const News = () => {
-  return <main></main>
+  return (
+    <main>
+      <HeadlineSection />
+    </main>
+  )
 }
 
 export default News
